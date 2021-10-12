e5b78af514e4120d379662e1e9d6973f3ca01dc4
jesus m. rodriguez <jesusr@redhat.com>
2021-10-11 21:23:54 -0400

    add comments

--- main.go
+++ main.go
@@ -9,12 +9,15 @@ import (
 func main() {
 	fs := afero.NewMemMapFs()
 
+	// create a file
 	_, err := fs.Create("/tmp/test.sh")
 	if err != nil {
 		fmt.Printf("%v\n", err)
 	}
+	// make it execuable
 	fs.Chmod("/tmp/test.sh", 0755)
 
+	// stat the file
 	fi, err := fs.Stat("/tmp/test.sh")
 	if err != nil {
 		fmt.Printf("%v\n", err)
@@ -23,16 +26,10 @@ func main() {
 	fmt.Printf("Fileinfo.size: %v\n", fi.Size())
 	fmt.Printf("Fileinfo.mode: %v\n", fi.Mode())
 
+	// see if file is executable
 	if fi.Mode()&0111 != 0 {
 		fmt.Println("File is executable")
 	} else {
 		fmt.Println("File is not executable")
 	}
-
-	// f, err := fs.FS.Create(filepath.Join(filepath.Dir(pluginFilePath), filename))
-	//         Expect(err).To(BeNil())
-	//         Expect(f).ToNot(BeNil())
-	//         // ensure that the file exists
-	//         _, err = fs.FS.Stat(pluginFilePath)
-	//         Expect(err).To(BeNil())
 }
